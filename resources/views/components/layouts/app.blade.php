027b5c2cf2ad032c2bd7de183ad169fb565ea63c
Ye Htun <yehtun438@gmail.com>
2024-11-06 16:09:08 +0630

    update cdn link

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link href="{{ asset('theme/css/app.css') }}" rel="stylesheet" />
 
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -39,15 +40,20 @@
 
     @livewireScripts
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script> --}}
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
-    </script>
+    </script> --}}
+    
     <script src="{{ asset('theme/plugins/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('theme/plugins/toastr/toastr.min.js') }}"></script>
     <script src="{{ asset('theme/js/app.js') }}"></script>
--- resources/views/livewire/navigation/side-navigation.blade.php
+++ resources/views/livewire/navigation/side-navigation.blade.php
@@ -10,7 +10,6 @@
             <li class="sidebar-item active">
             <li class="sidebar-item {{ Request::is('index') ? 'active' : '' }} " >
                 <a class="sidebar-link" href="{{ route('dashboard') }}" wire:navigate>
-                    {{-- <i class="align-middle" data-feather="sliders"></i> --}}
                     <i class="bi bi-binoculars-fill align-middle"></i>
                     <span class="align-middle">Dashboard</span>
                 </a>
@@ -18,7 +17,6 @@
 
             <li class="sidebar-item {{ Request::is('admin/user') ? 'active' : '' }}" >
                 <a class="sidebar-link" href="{{ route('admin.user') }}" wire:navigate >
-                    {{-- <i class="align-middle" data-feather="user"></i> --}}
                     <i class="bi bi-people-fill align-middle"></i>
                     <span class="align-middle">User</span>
                 </a>
@@ -26,27 +24,10 @@
 
             <li class="sidebar-item {{ Request::is('admin/posts') ? 'active' : '' }}" >
                 <a class="sidebar-link" href="{{ route('admin.posts') }}" wire:navigate>
-                    {{-- <i class="align-middle" data-feather="user"></i> --}}
                     <i class="bi bi-postcard-fill align-middle"></i>
                     <span class="align-middle">Post</span>
                 </a>
             </li>
-
-                <li class="sidebar-header">Auth</li>
-
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="{{ route('login') }}" >
-                        <i class="align-middle" data-feather="log-in"></i>
-                        <span class="align-middle">Sign In</span>
-                    </a>
-                </li>
-
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="{{ route('register') }}" >
-                        <i class="align-middle" data-feather="user-plus"></i>
-                        <span class="align-middle">Sign Up</span>
-                    </a>
-                </li>
             </ul>
         </div>
     </nav>
