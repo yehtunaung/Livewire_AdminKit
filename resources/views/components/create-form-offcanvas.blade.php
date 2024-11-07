0a54528789ff92b1f2e7175b60ae217309d09384
Ye Htun <yehtun438@gmail.com>
2024-11-08 00:13:38 +0630

    update offcanvas open responsive

--- resources/views/components/create-form-offcanvas.blade.php
+++ resources/views/components/create-form-offcanvas.blade.php
@@ -1,6 +1,6 @@
 @props(['submit','isOpen'])
-
-<div class="offcanvas {{ $isOpen ? 'show' : '' }}" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+<div class="offcanvas {{ $isOpen ? 'show' : '' }} position-absolute top-0 end-0  w-100 w-md-75 w-lg-50 w-xl-25 ms-md-4 ms-lg-5" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel" 
+    style="width: calc(100% - 250px); right: 0;">
 
     <div class="offcanvas-header">
         {{ $title }}
