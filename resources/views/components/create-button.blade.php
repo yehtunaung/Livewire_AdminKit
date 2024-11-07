c645e3e99d8ed4faf65ca1a19dd57721e3bdbcdc
Ye Htun <yehtun438@gmail.com>
2024-11-07 12:53:43 +0630

    update create button

--- resources/views/components/create-button.blade.php
+++ resources/views/components/create-button.blade.php
@@ -1,6 +1,6 @@
-
+@props(['title','function'])
 <div class="d-flex justify-content-end mb-3">
-    <x-success-button wire:click="create" wire:loading.attr="disabled">
-        {{ __('Create Post') }}
+    <x-success-button wire:click="{{$function}}" wire:loading.attr="disabled">
+        {{ __('Create') }}  {{ $title }}
     </x-success-button>
 </div>
