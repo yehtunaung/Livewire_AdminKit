706d9fe75cd2cd9c7034068419e2ff2cc1daf8c5
hlathinzarorient <hlathinzarnwe@orientwebs.com>
2024-11-11 09:57:35 +0630

    sss

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 <input {{ $attributes->merge(['type' => 'text', 'class' => 'form-control']) }}
     style="padding:8px; border-radius:7px; box-shadow:none;" wire:loading.attr="disabled" />
-{{ $slot }}
+{{ $slot }}   
