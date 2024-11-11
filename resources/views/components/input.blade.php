9ad441ec092b07ce0c1ed97bffa4b1d4eae8d24b
eaint_eaint_88 <ayemonmyint@orientwebs.com>
2024-11-11 09:58:49 +0630

    test

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,5 @@
 <input {{ $attributes->merge(['type' => 'text', 'class' => 'form-control']) }}
     style="padding:8px; border-radius:7px; box-shadow:none;" wire:loading.attr="disabled" />
 {{ $slot }}
+
+{{-- <h1>Hello World</h1> --}}
